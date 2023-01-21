e3c3ace99734596207842ad571f75b2082670bbd
Viktor Tytenko <92685344+ViktorTyt@users.noreply.github.com>
2023-01-21 14:45:11 +0200

    Initial, added authorization and user routers

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+const userRoute = require("./routers/users");
+const authRoute = require("./routers/auth");
+
+dotenv.config();
+
+mongoose.set("strictQuery", true);
+mongoose.connect(process.env.MONGO_URL, () => {
+  console.log("Mongo DB connected");
+});
+
+// middlewares
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+
+app.listen(8800, () => {
+  console.log("Backend server is running!");
+});
